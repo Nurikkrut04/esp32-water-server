ea3565f3a4604c5099c48679f21136bf7eff998c
Nurislam <nurikslam.beis@gmail.com>
2025-05-07 03:08:20 +0500

    Корректировки для вебсокета в socketio.run

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ def export_csv():
 # Запуск сервера
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
